ab271a18169414737246fb42738ac2941252cc12
Barney Boisvert <bboisvert@gmail.com>
2021-09-19 07:19:16 -0700

    use dynamic sorting

--- src/main/java/com/brennaswitzer/cookbook/repositories/InventoryTxRepository.kt
+++ src/main/java/com/brennaswitzer/cookbook/repositories/InventoryTxRepository.kt
@@ -2,9 +2,12 @@ package com.brennaswitzer.cookbook.repositories
 
 import com.brennaswitzer.cookbook.domain.InventoryItem
 import com.brennaswitzer.cookbook.domain.InventoryTx
+import org.springframework.data.domain.Sort
 
 interface InventoryTxRepository : BaseEntityRepository<InventoryTx> {
 
-    fun findByItemOrderByCreatedAt(item: InventoryItem): Iterable<InventoryTx>
+    fun findByItem(item: InventoryItem): Iterable<InventoryTx>
+
+    fun findByItem(item: InventoryItem, sort: Sort): Iterable<InventoryTx>
 
 }
