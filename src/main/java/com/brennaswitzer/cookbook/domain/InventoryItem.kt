c91f3f7b35b0c2f3b24c254612a8f70615856162
Barney Boisvert <bboisvert@gmail.com>
2021-09-19 09:41:51 -0700

    PoC the transaction log too

--- src/main/java/com/brennaswitzer/cookbook/domain/InventoryItem.kt
+++ src/main/java/com/brennaswitzer/cookbook/domain/InventoryItem.kt
@@ -1,24 +1,97 @@
 package com.brennaswitzer.cookbook.domain
 
-import javax.persistence.Embedded
-import javax.persistence.Entity
-import javax.persistence.ManyToOne
+import javax.persistence.*
 
 @Entity
-class InventoryItem : BaseEntity() {
+class InventoryItem : BaseEntity {
 
-    @ManyToOne
+    constructor() : super()
+
+    constructor(
+        user: User,
+        pantryItem: PantryItem,
+    ) : super() {
+        this.user = user
+        this.pantryItem = pantryItem
+    }
+
+    @ManyToOne(
+        fetch = FetchType.LAZY
+    )
     lateinit var user: User
 
     @ManyToOne
     lateinit var pantryItem: PantryItem
 
-    var storeOrder: Int? = null
+    @OneToMany(
+        targetEntity = InventoryTx::class,
+        orphanRemoval = true,
+        mappedBy = "item",
+        cascade = [CascadeType.ALL],
+        fetch = FetchType.LAZY
+    )
+    lateinit var transactions: MutableList<InventoryTx>
 
     /**
-     * This is a cache over the [InventoryTx] for this [PantryItem].
+     * This is a cache over [transactions].
      */
     @Embedded
-    lateinit var available: Quantity
+    var quantity: Quantity = Quantity.ZERO
+
+    /**
+     * This is a cache over [transactions].
+     */
+    var txCount: Int = 0
+
+    fun acquire(quantity: Quantity) {
+        addTransaction(
+            AcquireTx(
+                this,
+                quantity,
+            )
+        )
+    }
+
+    fun consume(quantity: Quantity) {
+        addTransaction(
+            ConsumeTx(
+                this,
+                quantity,
+            )
+        )
+    }
+
+    fun discard(quantity: Quantity) {
+        addTransaction(
+            DiscardTx(
+                this,
+                quantity,
+            )
+        )
+    }
+
+    fun reset(quantity: Quantity) {
+        addTransaction(
+            ResetTx(
+                this,
+                quantity,
+            )
+        )
+    }
+
+    private fun addTransaction(tx: InventoryTx) {
+        if (!this::transactions.isInitialized) {
+            this.transactions = mutableListOf(tx)
+        } else {
+            transactions.add(tx)
+        }
+        tx.commit()
+        quantity = tx.newQuantity
+        txCount += 1
+    }
+
+    override fun toString(): String {
+        return "InventoryItem(pantryItem=$pantryItem, available=$quantity)"
+    }
 
 }
--- src/main/java/com/brennaswitzer/cookbook/domain/InventoryTx.kt
+++ src/main/java/com/brennaswitzer/cookbook/domain/InventoryTx.kt
@@ -1,18 +1,121 @@
 package com.brennaswitzer.cookbook.domain
 
-import javax.persistence.Embedded
-import javax.persistence.Entity
-import javax.persistence.ManyToOne
+import javax.persistence.*
 
-@Entity
-class InventoryTx : BaseEntity() {
+@Entity // @MappedSuperclass can't back a polymorphic @Repository
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(discriminatorType = DiscriminatorType.INTEGER)
+abstract class InventoryTx : BaseEntity {
 
     @ManyToOne
     lateinit var item: InventoryItem
 
-    lateinit var action: InventoryAction
-
+    /**
+     * The quantity after the transaction.
+     */
     @Embedded
     lateinit var quantity: Quantity
 
+    constructor() : super()
+
+    constructor(
+        item: InventoryItem,
+        quantity: Quantity,
+    ) : super() {
+        this.item = item
+        this.quantity = quantity
+    }
+
+    /**
+     * The quantity before the transaction. [ResetTx] are destructive, so it is
+     * require to track for that type. However, it's useful for hypothetical
+     * "discard report" which wants to show only [DiscardTx] instance, but still
+     * wants to know the total inventory. Without tracking this, the full
+     * transaction would have to be processed.
+     */
+    @Embedded
+    @AttributeOverride(
+        name = "quantity",
+        column = Column(name = "new_quantity")
+    )
+    @AssociationOverride(
+        name = "units",
+        joinColumns = [JoinColumn(name = "new_units_id")]
+    )
+    var newQuantity: Quantity = Quantity.ZERO
+
+    final fun commit() {
+        newQuantity = computeNewQuantity(item.quantity)
+    }
+
+    abstract fun computeNewQuantity(curr: Quantity): Quantity
+
+    override fun toString(): String {
+        return "${this::class.simpleName}(quantity=$quantity, newQuantity=$newQuantity)"
+    }
+
+}
+
+/** You got some! */
+@Entity
+@DiscriminatorValue("1")
+class AcquireTx : InventoryTx {
+
+    constructor() : super()
+
+    constructor(
+        item: InventoryItem,
+        quantity: Quantity,
+    ) : super(item, quantity)
+
+    override fun computeNewQuantity(curr: Quantity) =
+        curr + quantity
+}
+
+/** You used some! */
+@Entity
+@DiscriminatorValue("2")
+class ConsumeTx : InventoryTx {
+
+    constructor() : super()
+
+    constructor(
+        item: InventoryItem,
+        quantity: Quantity,
+    ) : super(item, quantity)
+
+    override fun computeNewQuantity(curr: Quantity) =
+        curr - quantity
+}
+
+/** You threw some away! */
+@Entity
+@DiscriminatorValue("3")
+class DiscardTx : InventoryTx {
+
+    constructor() : super()
+
+    constructor(
+        item: InventoryItem,
+        quantity: Quantity,
+    ) : super(item, quantity)
+
+    override fun computeNewQuantity(curr: Quantity) =
+        curr - quantity
+}
+
+/** You lost track and need a reset! */
+@Entity
+@DiscriminatorValue("4")
+class ResetTx : InventoryTx {
+
+    constructor() : super()
+
+    constructor(
+        item: InventoryItem,
+        quantity: Quantity,
+    ) : super(item, quantity)
+
+    override fun computeNewQuantity(curr: Quantity) =
+        quantity
 }
