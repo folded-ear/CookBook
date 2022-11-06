52e0e6f6f61a7a17444875a7eec61c2d21074238
bswitzer <brenna.switzer@gmail.com>
2022-11-06 15:08:42 -0800

    adds comment

--- client/src/views/UserProfile/UserProfileView.js
+++ client/src/views/UserProfile/UserProfileView.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import PageBody from "../common/PageBody";
 import {UserProfileController} from "../../features/UserProfile/UserProfileController";
 
+// Not yet in use
 export const UserProfileView = () => {
     return (<PageBody><UserProfileController/></PageBody>);
 };
