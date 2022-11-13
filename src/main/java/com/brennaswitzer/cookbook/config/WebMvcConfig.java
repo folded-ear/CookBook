fa71e6cb9c82461effaee07e8777c70a8cd7ef74
Barney Boisvert <bboisvert@gmail.com>
2022-11-13 10:39:41 -0800

    tighten down CORS, now that we use it in prod

--- src/main/java/com/brennaswitzer/cookbook/config/WebMvcConfig.java
+++ src/main/java/com/brennaswitzer/cookbook/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.brennaswitzer.cookbook.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,14 +10,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private static final long MAX_AGE_SECS = 3600;
 
+    @Autowired
+    private AppProperties appProperties;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns(appProperties.getPublicUrl())
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
-
     }
+
 }
